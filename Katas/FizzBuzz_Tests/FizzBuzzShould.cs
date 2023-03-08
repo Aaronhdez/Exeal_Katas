15e54f872578853c0ab6bfb2720ed286466cab2c
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-08 10:49:46 +0000

    feat: extra cases added to test cases

--- Katas/FizzBuzz_Tests/FizzBuzzShould.cs
+++ Katas/FizzBuzz_Tests/FizzBuzzShould.cs
@@ -14,6 +14,9 @@ namespace FizzBuzz_Tests
         [TestCase(4, "4")]
         [TestCase(7, "7")]
         [TestCase(8, "8")]
+        [TestCase(11, "11")]
+        [TestCase(13, "13")]
+        [TestCase(14, "14")]
         public void Return_a_number_itself_when_is_not_divisible_neither_by_3_or_5(int number, string expectedResult)
         {
             var result = FizzBuzzGame.FizzBuzz(number);
@@ -24,6 +27,7 @@ namespace FizzBuzz_Tests
         [TestCase(3, "fizz")]
         [TestCase(6, "fizz")]
         [TestCase(9, "fizz")]
+        [TestCase(12, "fizz")]
         public void Return_fizz_when_a_number_is_divisible_by_3(int number, string expectedResult)
         {
             var result = FizzBuzzGame.FizzBuzz(number);
