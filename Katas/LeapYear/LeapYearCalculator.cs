908b9d21187cbe349d9fcad060b52ddd516da351
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-08 11:57:25 +0000

    refactor: methods extracted to improve legibility

--- Katas/LeapYear/LeapYearCalculator.cs
+++ Katas/LeapYear/LeapYearCalculator.cs
@@ -4,7 +4,21 @@ public class LeapYearCalculator
 {
     public static bool IsALeapYear(int year)
     {
-        return year % 400 == 0 ? true :
-            year % 4 == 0 && year % 100 != 0;
+        return IsDivisibleBy400(year) || IsDivisibleBy4(year) && !IsDivisibleBy100(year);
+    }
+
+    private static bool IsDivisibleBy400(int year)
+    {
+        return year % 400 == 0;
+    }
+
+    private static bool IsDivisibleBy100(int year)
+    {
+        return year % 100 == 0;
+    }
+
+    private static bool IsDivisibleBy4(int year)
+    {
+        return year % 4 == 0;
     }
 }
