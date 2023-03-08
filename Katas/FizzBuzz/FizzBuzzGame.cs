8d0908fe254a1d901977c085b35fd863d78740be
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-08 10:17:27 +0000

    refactor: uneccessary variable check removed

--- Katas/FizzBuzz/FizzBuzzGame.cs
+++ Katas/FizzBuzz/FizzBuzzGame.cs
@@ -5,7 +5,7 @@
         public static string FizzBuzz(int number)
         {
             if (number == 3) return "fizz";
-            return (number == 1) ? "1" : "2";
+            return number.ToString();
         }
     }
 }
