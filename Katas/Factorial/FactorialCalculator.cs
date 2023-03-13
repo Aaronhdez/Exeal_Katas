01b283657ad3cfbdc2201c6b7000dd5a5e16e787
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-13 08:29:13 +0000

    Feat_Return_1_if_input_is_0

---
+++ Katas/Factorial/FactorialCalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace Factorial;
+
+public class FactorialCalculator
+{
+    public static int Factorial(int number)
+    {
+        return 1;
+    }
+}
--- Katas/Factorial_Tests/FactorialCalculatorShould.cs
+++ Katas/Factorial_Tests/FactorialCalculatorShould.cs
@@ -1,5 +1,14 @@
-﻿namespace Factorial_Tests;
-public class FactorialCalculator
+﻿using Factorial;
+using FluentAssertions;
+
+namespace Factorial_Tests;
+public class FactorialCalculatorShould
 {
-    
+    [Test]
+    public void Return_1_if_input_is_0()
+    {
+        var result = FactorialCalculator.Factorial(0);
+        
+        result.Should().Be(1);
+    }
 }
