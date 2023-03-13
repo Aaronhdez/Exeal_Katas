55d7b0c311b9611204eaae00f1dd33a03fbf9891
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-13 09:10:31 +0000

    Refactor: if refactored to check higher than 1 condition

--- Katas/Fibonacci/FibonacciCalculator.cs
+++ Katas/Fibonacci/FibonacciCalculator.cs
@@ -4,9 +4,7 @@ public class FibonacciCalculator
 {
     public static int Fibonacci(int input)
     {
-        if (input == 1) return 1;
-        if (input == 2) return 1;
-        if (input == 3) return 2;
-        return 0;
+        if (input > 1) return input - 1;
+        return input;
     }
 }
