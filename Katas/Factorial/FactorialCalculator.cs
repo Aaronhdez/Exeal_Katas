449854310731c9efe3bc2c634feaf70385b5c7ef
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-13 08:42:10 +0000

    Refactor: if inverted to simple condition

--- Katas/Factorial/FactorialCalculator.cs
+++ Katas/Factorial/FactorialCalculator.cs
@@ -4,8 +4,7 @@ public class FactorialCalculator
 {
     public static int Factorial(int number)
     {
-        if (number >= 0 && number < 1)
-            return 1;
-        return number * Factorial(number - 1);
+        if (number != 0) return number * Factorial(number - 1);
+        return 1;
     }
 }
