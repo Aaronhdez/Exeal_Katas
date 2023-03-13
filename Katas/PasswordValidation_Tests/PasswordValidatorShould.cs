bd0a79b97c4f4efbecfba5cbf013f123488ff14a
cvigueras <carlosj.vigueras@gmail.com>
2023-03-13 11:30:05 +0100

    Feat: Return_error_message_if_password_not_contain_at_least_two_numbers

--- Katas/PasswordValidation_Tests/PasswordValidatorShould.cs
+++ Katas/PasswordValidation_Tests/PasswordValidatorShould.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using FluentAssertions;
 
 namespace PasswordValidation_Tests;
@@ -18,6 +19,16 @@ public class Tests
         
         result.Should().Be("Password must be al least 8 characters");
     }
+
+    [Test]
+    public void Return_error_message_if_password_not_contain_at_least_two_numbers()
+    {
+        var passwordValidator = new PasswordValidator();
+
+        var result = passwordValidator.Validate("gggg1jjaj");
+
+        result.Should().Be("The password must contain al least 2 numbers");
+    }
 }
 
 public class PasswordValidator
@@ -25,6 +36,8 @@ public class PasswordValidator
     public string Validate(string passwordToValidate)
     {
         if (passwordToValidate.Length < 8) return "Password must be al least 8 characters";
+        string numbersInPassword = Regex.Replace(passwordToValidate, @"[^\d]", String.Empty); ;
+        if (numbersInPassword.Length < 2) return "The password must contain al least 2 numbers";
         return "";
     }
 }
