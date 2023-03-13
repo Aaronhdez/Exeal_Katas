c7c862e5d1850a92b58fcb90ba1d8f819d95ec44
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-13 11:30:14 +0000

    Refactor: methods extracted un production code

--- Katas/PasswordValidation/PasswordValidator.cs
+++ Katas/PasswordValidation/PasswordValidator.cs
@@ -4,22 +4,29 @@ namespace PasswordValidation;
 
 public class PasswordValidator
 {
+    private const string PasswordMustBeAlLeastCharacters = "Password must be al least 8 characters";
+    private const string PasswordMustContainAlLeastNumbers = "Password must contain al least 2 numbers";
+
     public bool Validate(string passwordToValidate, out string validationResults)
     {
         validationResults = string.Empty;
-        if (passwordToValidate.Length < 8)
-        {
-            validationResults += "Password must be al least 8 characters";
-        }
-        var numbersInPassword = Regex.Replace(passwordToValidate, @"[^\d]", String.Empty); ;
-        if (numbersInPassword.Length < 2)
-        {
-            validationResults += (validationResults.Length == 0) ? 
-                "Password must contain al least 2 numbers":
-                "\nPassword must contain al least 2 numbers";
-        }
+        HasValidLength(passwordToValidate, ref validationResults);
+        HasAtLeastTwoNumbers(passwordToValidate, ref validationResults);
+        return validationResults.Length <= 0;
+    }
 
-        if (validationResults.Length > 0) return false;
-        return true;
+    private static void HasAtLeastTwoNumbers(string passwordToValidate, ref string validationResults)
+    {
+        var numbersInPassword = Regex.Replace(passwordToValidate, @"[^\d]", string.Empty);
+        if (numbersInPassword.Length < 2)
+            validationResults += (validationResults.Length == 0)
+                ? PasswordMustContainAlLeastNumbers
+                : $"\n{PasswordMustContainAlLeastNumbers}";
+    }
+
+    private static void HasValidLength(string passwordToValidate, ref string validationResults)
+    {
+        if (passwordToValidate.Length < 8)
+            validationResults += PasswordMustBeAlLeastCharacters;
     }
 }
