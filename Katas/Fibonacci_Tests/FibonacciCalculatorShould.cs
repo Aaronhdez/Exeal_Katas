93b1498305528f8bff928e78cae47c225e88cfe4
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-13 09:06:13 +0000

    Feat: Return_1_if_input_is_2

--- Katas/Fibonacci_Tests/FibonacciCalculatorShould.cs
+++ Katas/Fibonacci_Tests/FibonacciCalculatorShould.cs
@@ -20,4 +20,12 @@ public class FibonacciCalculatorShould
         
         result.Should().Be(1);
     }
+    
+    [Test]
+    public void Return_1_if_input_is_2()
+    {
+        var result = FibonacciCalculator.Fibonacci(2);
+        
+        result.Should().Be(1);
+    }
 }
