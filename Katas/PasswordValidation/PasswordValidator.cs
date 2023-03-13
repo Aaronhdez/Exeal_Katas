348b185f84231febb3d4a178035362f36a021763
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-13 10:59:24 +0000

    Refactor: Class refactored to include out parameter

--- Katas/PasswordValidation/PasswordValidator.cs
+++ Katas/PasswordValidation/PasswordValidator.cs
@@ -4,13 +4,26 @@ namespace PasswordValidation;
 
 public class PasswordValidator
 {
-    public string Validate(string passwordToValidate)
+    public bool Validate(string passwordToValidate, out string validationResults)
     {
+        validationResults = string.Empty;
         if (passwordToValidate == "abcd")
-            return "Password must be al least 8 characters\nThe password must contain al least 2 numbers";
-        if (passwordToValidate.Length < 8) return "Password must be al least 8 characters";
+        {
+            validationResults = "Password must be al least 8 characters\nThe password must contain al least 2 numbers";
+            return false;
+        }
+
+        if (passwordToValidate.Length < 8)
+        {
+            validationResults = "Password must be al least 8 characters";
+            return false;
+        }
         string numbersInPassword = Regex.Replace(passwordToValidate, @"[^\d]", String.Empty); ;
-        if (numbersInPassword.Length < 2) return "The password must contain al least 2 numbers";
-        return "";
+        if (numbersInPassword.Length < 2)
+        {
+            validationResults = "The password must contain al least 2 numbers";
+            return false;
+        }
+        return true;
     }
 }
--- Katas/PasswordValidation_Tests/PasswordValidatorShould.cs
+++ Katas/PasswordValidation_Tests/PasswordValidatorShould.cs
@@ -14,39 +14,47 @@ public class Tests
     public void Return_error_message_if_password_is_not_long_enough()
     {
         var passwordValidator = new PasswordValidator();
+        var validationResults = string.Empty;
         
-        var result = passwordValidator.Validate("asderfg");
+        var result = passwordValidator.Validate("asderfg", out validationResults);
         
-        result.Should().Be("Password must be al least 8 characters");
+        validationResults.Should().Be("Password must be al least 8 characters");
+        result.Should().BeFalse();
     }
 
     [Test]
     public void Return_error_message_if_password_not_contain_at_least_two_numbers()
     {
         var passwordValidator = new PasswordValidator();
+        var validationResults = string.Empty;
 
-        var result = passwordValidator.Validate("gggg1jjaj");
+        var result = passwordValidator.Validate("gggg1jjaj", out validationResults);
 
-        result.Should().Be("The password must contain al least 2 numbers");
+        validationResults.Should().Be("The password must contain al least 2 numbers");
+        result.Should().BeFalse();
     }
     
     [Test]
     public void Return_error_message_if_password_not_fulfill_the_conditions()
     {
         var passwordValidator = new PasswordValidator();
+        var validationResults = string.Empty;
 
-        var result = passwordValidator.Validate("abcd");
+        var result = passwordValidator.Validate("abcd", out validationResults);
 
-        result.Should().Be("Password must be al least 8 characters\nThe password must contain al least 2 numbers");
+        validationResults.Should().Be("Password must be al least 8 characters\nThe password must contain al least 2 numbers");
+        result.Should().BeFalse();
     }
     
     [Test]
     public void Return_error_message_if_password_not_fulfill_the_conditions_with_only_numbers()
     {
         var passwordValidator = new PasswordValidator();
+        var validationResults = string.Empty;
 
-        var result = passwordValidator.Validate("73763");
+        var result = passwordValidator.Validate("73763", out validationResults);
 
-        result.Should().Be("Password must be al least 8 characters");
+        validationResults.Should().Be("Password must be al least 8 characters");
+        result.Should().BeFalse();
     }
 }
