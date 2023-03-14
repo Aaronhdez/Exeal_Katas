1963b5664d021366bda6a45fe0b1689ec8b80784
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-14 10:45:08 +0000

    Refactor: class extracted to file

---
+++ Katas/TicTacToe/Coordinates.cs
@@ -0,0 +1,18 @@
+﻿namespace TicTacToe.Test;
+
+public class Coordinates
+{
+    private readonly int _x;
+    private readonly int _y;
+    
+    public Coordinates(int x, int y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    public override string ToString()
+    {
+        return _x + "," + _y;
+    }
+}
--- Katas/TicTacToe/TicTacToeGame.cs
+++ Katas/TicTacToe/TicTacToeGame.cs
@@ -1,22 +1,5 @@
 ﻿namespace TicTacToe.Test;
 
-public class Coordinates
-{
-    public Coordinates(int x, int y)
-    {
-        _x = x;
-        _y = y;
-    }
-
-    public override string ToString()
-    {
-        return _x + "," + _y;
-    }
-
-    private readonly int _x;
-    private readonly int _y;
-}
-
 public class TicTacToeGame
 {
     private static readonly Dictionary<string,string> board = new Dictionary<string, string>();
