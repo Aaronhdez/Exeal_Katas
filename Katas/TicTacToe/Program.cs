2268a8a1690a354d3a9dfdf27a4da2fa4554e13f
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-15 11:23:52 +0000

    Feat: added UI to the game

--- Katas/TicTacToe/Program.cs
+++ Katas/TicTacToe/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TicTacToe;
+﻿using TicTacToe.Model;
+
+namespace TicTacToe;
 
 public class Program
 {
@@ -6,10 +8,28 @@ public class Program
     {
         var ticTacToeGame = new TicTacToeGame();
         Console.WriteLine("TicTacToe!");
-        Console.Write($"{ticTacToeGame.CurrentPlayer()} Goes...");
-        Console.WriteLine("Coordinate x");
-        Console.ReadLine();
-        Console.WriteLine("Coordinate Y");
-        Console.WriteLine();
+        while (true)
+        {
+            Console.Write($"{ticTacToeGame.CurrentPlayer()} Goes...");
+            Console.WriteLine("Coordinate X");
+            var coodinateX = int.Parse(Console.ReadLine() ?? "0");
+            Console.WriteLine("Coordinate Y");
+            var coodinateY = int.Parse(Console.ReadLine() ?? "0");
+            Console.WriteLine();
+            try
+            {
+                ticTacToeGame.Write(
+                    new Symbol(ticTacToeGame.CurrentPlayer()), 
+                    new Coordinates(coodinateX,coodinateY));
+                Console.WriteLine(ticTacToeGame.CurrentStatus());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("That cell is already taken!, try again");
+                Console.ReadLine();
+                throw;
+            }
+        }
+        
     }
 }
