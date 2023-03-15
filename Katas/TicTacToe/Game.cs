c67104185b3e012362e98401acc30ef8dcb3f3f5
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-15 15:05:36 +0000

    Refactor: logic methods extracted from Board to Game

--- Katas/TicTacToe/Game.cs
+++ Katas/TicTacToe/Game.cs
@@ -37,16 +37,16 @@ public class Game
 
     private bool PlayerXHasWon()
     {
-        return _board.AColumnHasBeenFilledBy(XPlayer) || 
-               _board.ARowHasBeenFilledBy(XPlayer) ||
-               _board.ADiagonalRowHasBeenFilledBy(XPlayer);
+        return AColumnHasBeenFilledBy(XPlayer) || 
+               ARowHasBeenFilledBy(XPlayer) ||
+               ADiagonalRowHasBeenFilledBy(XPlayer);
     }
 
     private bool PlayerOHasWon()
     {
-        return _board.AColumnHasBeenFilledBy(OPlayer) || 
-               _board.ARowHasBeenFilledBy(OPlayer) ||
-               _board.ADiagonalRowHasBeenFilledBy(OPlayer);
+        return AColumnHasBeenFilledBy(OPlayer) || 
+               ARowHasBeenFilledBy(OPlayer) ||
+               ADiagonalRowHasBeenFilledBy(OPlayer);
     }
 
     private string FormattedBoardStatus()
@@ -60,5 +60,50 @@ public class Game
     public string CurrentPlayer()
     {
         return _currentPlayer;
+    }
+
+    private bool AColumnHasBeenFilledBy(string player)
+    {
+        for (var y = 0; y < 3; y++)
+        {
+            if (_board.TokenAt(new Coordinates(0, y)) == player
+                && _board.TokenAt(new Coordinates(0, y)) ==
+                _board.TokenAt(new Coordinates(1, y))
+                && _board.TokenAt(new Coordinates(1, y)) ==
+                _board.TokenAt(new Coordinates(2, y)))
+                return true;
+        }
+        return false;
+    }
+
+    private bool ARowHasBeenFilledBy(string player)
+    {
+        for (var x = 0; x < 3; x++)
+        {
+            if (_board.TokenAt(new Coordinates(x, 2)) == player
+                && _board.TokenAt(new Coordinates(x, 2)) ==
+                _board.TokenAt(new Coordinates(x, 1))
+                && _board.TokenAt(new Coordinates(x, 1)) ==
+                _board.TokenAt(new Coordinates(x, 0)))
+                return true;
+        }
+        return false;
+    }
+
+    private bool ADiagonalRowHasBeenFilledBy(string player)
+    {
+        if (_board.TokenAt(new Coordinates(2, 2)) == player
+            && _board.TokenAt(new Coordinates(2, 2)) ==
+            _board.TokenAt(new Coordinates(1, 1))
+            && _board.TokenAt(new Coordinates(1, 1)) ==
+            _board.TokenAt(new Coordinates(0, 0)))
+            return true;
+        if (_board.TokenAt(new Coordinates(0, 2)) == player
+            && _board.TokenAt(new Coordinates(0, 2)) ==
+            _board.TokenAt(new Coordinates(1, 1))
+            && _board.TokenAt(new Coordinates(1, 1)) ==
+            _board.TokenAt(new Coordinates(2, 0)))
+            return true;
+        return false;
     }
 }
--- Katas/TicTacToe/Model/Board.cs
+++ Katas/TicTacToe/Model/Board.cs
@@ -31,49 +31,4 @@ public class Board
         return _board.ContainsKey(coordinates.ToString()) ? 
             _board[coordinates.ToString()] : string.Empty;
     }
-
-    public bool AColumnHasBeenFilledBy(string player)
-    {
-        for (var y = 0; y < 3; y++)
-        {
-            if (TokenAt(new Coordinates(0, y)) == player
-                && TokenAt(new Coordinates(0, y)) ==
-                TokenAt(new Coordinates(1, y))
-                && TokenAt(new Coordinates(1, y)) ==
-                TokenAt(new Coordinates(2, y)))
-                return true;
-        }
-        return false;
-    }
-
-    public bool ARowHasBeenFilledBy(string player)
-    {
-        for (var x = 0; x < 3; x++)
-        {
-            if (TokenAt(new Coordinates(x, 2)) == player
-                && TokenAt(new Coordinates(x, 2)) ==
-                TokenAt(new Coordinates(x, 1))
-                && TokenAt(new Coordinates(x, 1)) ==
-                TokenAt(new Coordinates(x, 0)))
-                return true;
-        }
-        return false;
-    }
-
-    public bool ADiagonalRowHasBeenFilledBy(string player)
-    {
-        if (TokenAt(new Coordinates(2, 2)) == player
-            && TokenAt(new Coordinates(2, 2)) ==
-            TokenAt(new Coordinates(1, 1))
-            && TokenAt(new Coordinates(1, 1)) ==
-            TokenAt(new Coordinates(0, 0)))
-            return true;
-        if (TokenAt(new Coordinates(0, 2)) == player
-            && TokenAt(new Coordinates(0, 2)) ==
-            TokenAt(new Coordinates(1, 1))
-            && TokenAt(new Coordinates(1, 1)) ==
-            TokenAt(new Coordinates(2, 0)))
-            return true;
-        return false;
-    }
 }
