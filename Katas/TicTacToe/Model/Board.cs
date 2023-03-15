49fbbfcfa439706944ec29b00c828817e34e1f37
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-15 15:05:36 +0000

    Refactor: logic methods extracted from Board to Game

--- Katas/TicTacToe/Model/Board.cs
+++ Katas/TicTacToe/Model/Board.cs
@@ -2,78 +2,33 @@
 
 public class Board
 {
-    private readonly Dictionary<string, string> _board;
+    private readonly Dictionary<string, string> _cells;
 
     public Board()
     {
-        _board = new Dictionary<string, string>();
+        _cells = new Dictionary<string, string>();
     }
 
     public bool IsEmpty()
     {
-        return _board.Count == 0;
+        return _cells.Count == 0;
     }
 
     public bool IsFilled()
     {
-        return _board.Count == 9;
+        return _cells.Count == 9;
     }
 
     public void WriteASymbol(Symbol symbol, Coordinates coordinates)
     {
         if (!coordinates.AreValid()) throw new IndexOutOfRangeException();
-        if (!_board.ContainsKey(coordinates.ToString()))
-            _board.Add(coordinates.ToString(), symbol.Value);
+        if (!_cells.ContainsKey(coordinates.ToString()))
+            _cells.Add(coordinates.ToString(), symbol.Value);
     }
 
     public string TokenAt(Coordinates coordinates)
     {
-        return _board.ContainsKey(coordinates.ToString()) ? 
-            _board[coordinates.ToString()] : string.Empty;
-    }
-
-    public bool AColumnHasBeenFilledBy(string player)
-    {
-        for (var y = 0; y < 3; y++)
-        {
-            if (TokenAt(new Coordinates(0, y)) == player
-                && TokenAt(new Coordinates(0, y)) ==
-                TokenAt(new Coordinates(1, y))
-                && TokenAt(new Coordinates(1, y)) ==
-                TokenAt(new Coordinates(2, y)))
-                return true;
-        }
-        return false;
-    }
-
-    public bool ARowHasBeenFilledBy(string player)
-    {
-        for (var x = 0; x < 3; x++)
-        {
-            if (TokenAt(new Coordinates(x, 2)) == player
-                && TokenAt(new Coordinates(x, 2)) ==
-                TokenAt(new Coordinates(x, 1))
-                && TokenAt(new Coordinates(x, 1)) ==
-                TokenAt(new Coordinates(x, 0)))
-                return true;
-        }
-        return false;
-    }
-
-    public bool ADiagonalRowHasBeenFilledBy(string player)
-    {
-        if (TokenAt(new Coordinates(2, 2)) == player
-            && TokenAt(new Coordinates(2, 2)) ==
-            TokenAt(new Coordinates(1, 1))
-            && TokenAt(new Coordinates(1, 1)) ==
-            TokenAt(new Coordinates(0, 0)))
-            return true;
-        if (TokenAt(new Coordinates(0, 2)) == player
-            && TokenAt(new Coordinates(0, 2)) ==
-            TokenAt(new Coordinates(1, 1))
-            && TokenAt(new Coordinates(1, 1)) ==
-            TokenAt(new Coordinates(2, 0)))
-            return true;
-        return false;
+        return _cells.ContainsKey(coordinates.ToString()) ? 
+            _cells[coordinates.ToString()] : string.Empty;
     }
 }
