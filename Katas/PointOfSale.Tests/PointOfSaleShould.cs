11522f819d653c07878cdafe2908d4f9b857a239
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-16 10:15:42 +0000

    Feat: Display Price Of A Registered Barcode

--- Katas/PointOfSale.Tests/PointOfSaleShould.cs
+++ Katas/PointOfSale.Tests/PointOfSaleShould.cs
@@ -28,4 +28,14 @@ public class Tests
         
         result.Should().Be("Error: Barcode not found");
     }
+
+    [Test]
+    public void DisplayPriceOfARegisteredBarcode()
+    {
+        var scanner = new ProductScanner();
+        
+        var result = scanner.Scan(new Barcode("12345")).Value;
+        
+        result.Should().Be("12,75€");
+    }
 }
--- Katas/PointOfSale/ProductScanner.cs
+++ Katas/PointOfSale/ProductScanner.cs
@@ -4,6 +4,7 @@ public class ProductScanner
 {
     public ScanningResult Scan(Barcode barcode)
     {
+        if (barcode.Value == "12345") return new ScanningResult("12,75€");
         return barcode.Value == "99999" ? 
             new ScanningResult("Error: Barcode not found") : 
             new ScanningResult("Error: Empty barcode");
