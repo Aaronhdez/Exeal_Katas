61aecefe544b7d2a3eb1a306941ca934c7300e22
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-16 11:27:52 +0000

    Refactor: tests refactored to Test Cases

--- Katas/StringCalculator/StringCalculator_Test/StringCalculatorMachineShould.cs
+++ Katas/StringCalculator/StringCalculator_Test/StringCalculatorMachineShould.cs
@@ -28,27 +28,13 @@ public class StringCalculatorMachineShould
         result.Should().Be(expectedResult);
     }
 
-    [Test]
-    public void GiveTotalResultForTwoNumbers()
+    [TestCase("1,2", 3)]
+    [TestCase("2,2", 4)]
+    [TestCase("2,3", 5)]
+    public void GiveTotalResultForAnyPairOfNumbers(string input, int expectedResult)
     {
-        var result = StringCalculatorMachine.Add("1,2");
+        var result = StringCalculatorMachine.Add(input);
         
-        result.Should().Be(3);
-    }
-
-    [Test]
-    public void GiveTotalResultForOtherTwoNumbers()
-    {
-        var result = StringCalculatorMachine.Add("2,2");
-        
-        result.Should().Be(4);
-    }
-
-    [Test]
-    public void GiveTotalResultForAnotherTwoNumbers()
-    {
-        var result = StringCalculatorMachine.Add("2,3");
-        
-        result.Should().Be(5);
+        result.Should().Be(expectedResult);
     }
 }
