8977d0a74fbdcfa45af4eb3656a29a07d728e02b
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-16 11:22:20 +0000

    Refactor: tests refactored to test cases

--- Katas/StringCalculator/StringCalculator_Test/StringCalculatorMachineShould.cs
+++ Katas/StringCalculator/StringCalculator_Test/StringCalculatorMachineShould.cs
@@ -17,11 +17,14 @@ public class StringCalculatorMachineShould
         result.Should().Be(0);
     }
     
-    [Test]
-    public void GiveSameNumberAsAResultForASingleNumber()
+    [TestCase("1", 1)]
+    [TestCase("2", 2)]
+    [TestCase("12", 12)]
+    [TestCase("121", 121)]
+    public void GiveSameNumberAsAResultForASingleNumber(string input, int expectedResult)
     {
-        var result = StringCalculatorMachine.Add("1");
+        var result = StringCalculatorMachine.Add(input);
         
-        result.Should().Be(1);
+        result.Should().Be(expectedResult);
     }
 }
