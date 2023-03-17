dc1fad6606db7bdcea5da0ab428ac1f02e7c0344
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-17 15:09:58 +0000

    Chore: added search functionality assets

---
+++ Katas/SearchEngine.Tests/SearchEngineShould.cs
@@ -0,0 +1,15 @@
+namespace SearchEngine.Tests;
+
+public class SearchEngineShould
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
