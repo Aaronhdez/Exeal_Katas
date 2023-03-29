7335f700ce465978c59e1e9cd2bdd6d55bd434d5
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-03-29 09:53:26 +0100

    Chore: added basic infrastructure for Mars Rover Kata

---
+++ Katas/MarsRover.Tests/MarsRoverShould.cs
@@ -0,0 +1,15 @@
+namespace MarsRover.Tests;
+
+public class MarsRoverShould
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void LandInSpecificCoordinates()
+    {
+        Assert.Pass();
+    }
+}
