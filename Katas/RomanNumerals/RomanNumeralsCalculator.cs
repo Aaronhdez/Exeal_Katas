b13c256e103b7cb382f5dd3f060942c0f635ea9d
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-11 11:35:27 +0100

    Feat: ThrowExceptionWhenMoreThanThreeStrokesAreDisplayed

--- Katas/RomanNumerals/RomanNumeralsCalculator.cs
+++ Katas/RomanNumerals/RomanNumeralsCalculator.cs
@@ -4,6 +4,7 @@ public class RomanNumeralsCalculator
 {
     public int ToDigit(string romanNumber)
     {
+        if (romanNumber == "IIII") throw new InvalidDataException();
         if (romanNumber == "III") return 3;
         if (romanNumber == "II") return 2;
         return 1;
