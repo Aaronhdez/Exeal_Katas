658ecc18ca6f951abb36cb5587c3cb9e438bc7f5
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-14 13:45:16 +0100

    Feat: Return 500 For A D

--- Katas/RomanNumerals/RomanNumber.cs
+++ Katas/RomanNumerals/RomanNumber.cs
@@ -43,6 +43,7 @@ public class RomanNumber
         if (c == 'X') return 10;
         if (c == 'L') return 50;        
         if (c == 'C') return 100;
+        if (c == 'D') return 500;
         return 1;
     }
 }
