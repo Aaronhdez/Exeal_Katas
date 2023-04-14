ebf742cef9910f41d276fb6f66adb14f645fd8d5
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-14 14:11:47 +0100

    Refactor: Arrays extracted to fields in number Validator

--- Katas/RomanNumerals/RomanNumberValidator.cs
+++ Katas/RomanNumerals/RomanNumberValidator.cs
@@ -2,6 +2,9 @@
 
 public class RomanNumberValidator
 {
+    private readonly char[] _multiplesOf5 = { 'V', 'L', 'D'};
+    private readonly char[] _multiplesOf10 = { 'I', 'X', 'C'};
+
     public bool Validate(string value)
     {
         if (!ValidAmountsOfMultiplesOf10(value)) throw new InvalidDataException();
@@ -11,13 +14,11 @@ public class RomanNumberValidator
 
     private bool ValidAmountsOfMultiplesOf5(string value)
     {
-        var multiplesOf5 = new[] { 'I', 'X', 'C'};
-        return !multiplesOf5.Any(character => value.Count(c => c == character) <= 3);
+        return !_multiplesOf5.Any(character => value.Count(c => c == character) <= 3);
     }
 
     private bool ValidAmountsOfMultiplesOf10(string value)
     {
-        var multiplesOf10 = new[] { 'I', 'X', 'C'};
-        return !multiplesOf10.Any(character => value.Count(c => c == character) <= 3);
+        return !_multiplesOf10.Any(character => value.Count(c => c == character) <= 3);
     }
 }
