f8bdb9473685768c9cd6e25ddd8c011a2bda1e95
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-14 13:59:26 +0100

    Refactor: extracted roman numbers validator due to functionality accumulation in RomanNumber.cs

--- Katas/RomanNumerals/RomanNumber.cs
+++ Katas/RomanNumerals/RomanNumber.cs
@@ -1,8 +1,33 @@
 ﻿namespace RomanNumerals;
 
+public class RomanNumberValidator
+{
+    public bool Validate(string value)
+    {
+        if (!ValidAmountsOfMultiplesOf10(value)) return false;
+        if (!ValidAmountsOfMultiplesOf5(value)) return false;
+        return true;
+    }
+
+    private bool ValidAmountsOfMultiplesOf5(string value)
+    {
+        return value.Count(c => c == 'V') <= 1
+               && value.Count(c => c == 'L') <= 1
+               && value.Count(c => c == 'D') <= 1;
+    }
+
+    private bool ValidAmountsOfMultiplesOf10(string value)
+    {
+        return value.Count(c => c == 'I') <= 3
+               && value.Count(c => c == 'X') <= 3
+               && value.Count(c => c == 'C') <= 3;
+    }
+}
+
 public class RomanNumber
 {
     private readonly string _value;
+    private readonly RomanNumberValidator _validator;
 
     private Dictionary<char, int> _intEquivalences = new()
     {
@@ -12,33 +37,13 @@ public class RomanNumber
     public RomanNumber(string value)
     {
         _value = value;
+        _validator = new RomanNumberValidator();
     }
 
     public int ToDigit()
     {
-        if (!IsValid(_value)) throw new InvalidDataException();
+        if (!_validator.Validate(_value)) throw new InvalidDataException();
         return SumOfNumbers(_value);
-    }
-
-    private bool IsValid(string value)
-    {
-        if (!ValidMultipleOf10(value)) return false;
-        if (!ValidMultipleOf5(value)) return false;
-        return true;
-    }
-
-    private static bool ValidMultipleOf5(string value)
-    {
-        return value.Count(c => c == 'V') <= 1 
-               && value.Count(c => c == 'L') <= 1 
-               && value.Count(c => c == 'D') <= 1;
-    }
-
-    private static bool ValidMultipleOf10(string value)
-    {
-        return value.Count(c => c == 'I') <= 3 
-               && value.Count(c => c == 'X') <= 3 
-               && value.Count(c => c == 'C') <= 3;
     }
 
     private int SumOfNumbers(string value)
