d41608d12a83595f5c90a63ee0a00889d6563715
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-17 08:57:32 +0100

    Test: Throw Exception When More Than Three Ms Are Displayed

--- Katas/RomanNumerals.Tests/RomanNumberShould.cs
+++ Katas/RomanNumerals.Tests/RomanNumberShould.cs
@@ -90,12 +90,13 @@ public class RomanNumberShould
     [TestCase("IIII")]
     [TestCase("XXXX")]
     [TestCase("CCCC")]
+    [TestCase("MMMM")]
     [TestCase("VV")]
     [TestCase("LL")]
     [TestCase("DD")]
     public void ThrowExceptionWhenValueProvidedIsNotValid(string input)
     {
-        var result = () => new RomanNumber(input);;
+        var result = () => new RomanNumber(input);
         
         result.Should().Throw<InvalidDataException>();
     }
