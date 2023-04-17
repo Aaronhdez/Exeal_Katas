57ecc919dccc2f99d2aad31fa359cdcfbe767218
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-17 11:21:08 +0100

    Feat: added console project to try Roman numerals conversion

---
+++ Katas/RomanNumerals.Example/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RomanNumerals;
+
+while (true)
+{
+    Console.WriteLine("Please, provide a roman numeral");
+    var input = Console.ReadLine();
+    try
+    {
+        var numberAsDigit = new RomanNumber(input ?? "").ToDigit();
+        Console.WriteLine($"Value of '{input}' is {numberAsDigit}\n");
+    }
+    catch (InvalidDataException exception)
+    {
+        Console.WriteLine($"{exception}: '{input}' is not a valid roman numeral\n");
+    }
+}
