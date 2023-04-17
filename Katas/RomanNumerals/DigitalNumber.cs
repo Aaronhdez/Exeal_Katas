5dc518f661c4ae738d7e25f0c7b24d6184bfc41a
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-17 11:56:19 +0100

    Feat: Return Two Strokes For 2

--- Katas/RomanNumerals/DigitalNumber.cs
+++ Katas/RomanNumerals/DigitalNumber.cs
@@ -2,13 +2,16 @@
 
 public class DigitalNumber
 {
+    private readonly int _value;
+
     public DigitalNumber(string digit)
     {
-        
+        _value = int.Parse(digit);
     }
 
     public string ToRomanNumeral()
     {
+        if (_value == 2) return "II";
         return "I";
     }
 }
