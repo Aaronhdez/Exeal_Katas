7ce6baceb1d720fd1edbfa1d33f43609e19b33f0
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-18 08:38:32 +0100

    Refactor: strokes merged with edge combination testcases

--- Katas/RomanNumerals.Tests/DigitalNumberShould.cs
+++ Katas/RomanNumerals.Tests/DigitalNumberShould.cs
@@ -9,15 +9,6 @@ public class DigitalNumberShould
     [TestCase("1", "I")]
     [TestCase("2", "II")]
     [TestCase("3", "III")]
-    public void ReturnEquivalencesToStrokes(string input, string expectedResult)
-    {
-        _digitalNumber = new DigitalNumber(input);
-
-        var result = _digitalNumber.ToRomanNumeral();
-
-        result.Should().Be(expectedResult);
-    }
-
     [TestCase("4", "IV")]
     [TestCase("9", "IX")]
     [TestCase("40", "XL")]
