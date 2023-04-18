8a2d030ee89c097411373dda0f3d6ea0d397eab6
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-18 08:35:33 +0100

    Refactor: multiples of 10 tests refactored into testCases

--- Katas/RomanNumerals.Tests/DigitalNumberShould.cs
+++ Katas/RomanNumerals.Tests/DigitalNumberShould.cs
@@ -33,6 +33,18 @@ public class DigitalNumberShould
 
         result.Should().Be(expectedResult);
     }
+    
+    [TestCase("10", "X")]
+    [TestCase("100", "C")]
+    [TestCase("1000", "M")]
+    public void ReturnEquivalencesToMutiplesOf10(string input, string expectedResult)
+    {
+        _digitalNumber = new DigitalNumber(input);
+
+        var result = _digitalNumber.ToRomanNumeral();
+
+        result.Should().Be(expectedResult);
+    }
 
     [Test]
     public void ReturnVForA5()
@@ -42,16 +54,6 @@ public class DigitalNumberShould
         var result = _digitalNumber.ToRomanNumeral();
 
         result.Should().Be("V");
-    }
-
-    [Test]
-    public void ReturnXFor10()
-    {
-        _digitalNumber = new DigitalNumber("10");
-
-        var result = _digitalNumber.ToRomanNumeral();
-
-        result.Should().Be("X");
     }
     
     [Test]
@@ -63,16 +65,6 @@ public class DigitalNumberShould
 
         result.Should().Be("L");
     }
-    
-    [Test]
-    public void ReturnCFor100()
-    {
-        _digitalNumber = new DigitalNumber("100");
-
-        var result = _digitalNumber.ToRomanNumeral();
-
-        result.Should().Be("C");
-    }
     [Test]
     public void ReturnCFor500()
     {
@@ -81,15 +73,5 @@ public class DigitalNumberShould
         var result = _digitalNumber.ToRomanNumeral();
 
         result.Should().Be("C");
-    }
-    
-    [Test]
-    public void ReturnMFor1000()
-    {
-        _digitalNumber = new DigitalNumber("1000");
-
-        var result = _digitalNumber.ToRomanNumeral();
-
-        result.Should().Be("M");
     }
 }
