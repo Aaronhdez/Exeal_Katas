cbc1b0a49249685ce365dca8ffd89c7c3099273f
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-18 11:11:54 +0100

    Refactor: demo now includes dual conversion

--- Katas/RomanNumerals.Example/Program.cs
+++ Katas/RomanNumerals.Example/Program.cs
@@ -2,17 +2,57 @@
 
 using RomanNumerals;
 
-while (true)
+class Demo
 {
-    Console.WriteLine("Please, provide a roman numeral");
-    var input = Console.ReadLine();
-    try
+    public static void Main(string[] args)
     {
-        var numberAsDigit = new RomanNumber(input ?? "").ToDigit();
-        Console.WriteLine($"Value of '{input}' is {numberAsDigit}\n");
+        while (true)
+        {
+            Console.WriteLine("Write A to convert from Roman to Integer");
+            Console.WriteLine("Write B to convert from Integer to Roman ");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "A":
+                    ToDigit();
+                    break;
+                case "B":
+                    ToRoman();
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
     }
-    catch (InvalidDataException exception)
+
+    private static void ToDigit()
     {
-        Console.WriteLine($"{exception}: '{input}' is not a valid roman numeral\n");
+        Console.WriteLine("Provide a Number");
+        var input = Console.ReadLine();
+        try
+        {
+            var number = new RomanNumber(input ?? "").ToDigit();
+            Console.WriteLine($"Value of '{input}' is {number}\n");
+        }
+        catch (InvalidDataException exception)
+        {
+            Console.WriteLine($"{exception}: '{input}' is not a valid roman numeral\n");
+        }
+    }
+    
+    private static void ToRoman()
+    {
+        Console.WriteLine("Provide a Number");
+        var input = Console.ReadLine();
+        try
+        {
+            var digitalNumber = new DigitalNumber(int.Parse(input ?? "")).ToRomanNumeral();
+            Console.WriteLine($"Value of '{input}' is {digitalNumber}\n");
+        }
+        catch (InvalidDataException exception)
+        {
+            Console.WriteLine($"{exception}: '{input}' is not a valid digital numeral\n");
+        }
     }
 }
