66f6143c7f19fb1f7a4189e3164b05d875f8cb5f
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-18 10:26:35 +0100

    Test: Return Roman Value For An Integer

---
+++ Katas/RomanNumerals.Tests/NumberConverterShould.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace RomanNumerals.Tests;
+
+public class NumberConverterShould
+{
+    [Test]
+    public void ReturnRomanValueForAnInteger()
+    {
+        var result = NumberConverter.ToRomanNumeral(2023);
+        
+        result.Should().Be("MMXXIII");
+    }
+}
+
+public static class NumberConverter
+{
+    public static string ToRomanNumeral(int number)
+    {
+        return "";
+    }
+}
