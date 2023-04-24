51242a9a13ac7ec1322c6a9b5139c41801fb8d5d
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-24 15:26:25 +0100

    Test: Display basic information of an order

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Acceptance/DisplayFeature.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace OrdersWebApi.Tests.Acceptance;
+
+public class DisplayFeature
+{
+    [Test]
+    public async Task DisplayBasicInformationOfAnOrder()
+    {
+        var socialNetworkApp = new OrdersApi();
+        var client = socialNetworkApp.CreateClient();
+        var jsonPost = "{\"id\": \"ORD123456\"," +
+                            "\"customer\": \"John Doe\"," +
+                            "\"address\": \"A Simple Street, 123\","  +
+                            "\"products\": [" +
+                              "\"product\":" +
+                                "\"name\": \"Computer Monitor\"," +
+                                "\"price\": \"100€\"" +
+                              "\"product\":" +
+                                "\"name\": \"Keyboard\"," + 
+                                "\"price\": \"30€\"" +
+                            "]}";
+
+        var postResponse = await client.PostAsync("/Orders/ORD123456", new StringContent(jsonPost, Encoding.Default, "application/json"));
+        postResponse.EnsureSuccessStatusCode();
+        var response = await client.GetAsync("/Orders/ORD123456");
+        response.EnsureSuccessStatusCode();
+        
+        var content = await response.Content.ReadAsStringAsync();
+        var json = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(content), Formatting.Indented);
+        
+        await Verifier.Verify(json);
+    }
+}
