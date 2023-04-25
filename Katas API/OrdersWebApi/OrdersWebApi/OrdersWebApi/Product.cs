4e8a82bfa16cc9383fe69bfd4d2b2d9d8c5bc5b4
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-25 09:44:44 +0100

    Feat: Create A New Order With Products List

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Product.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Product.cs
@@ -11,4 +11,22 @@ public class Product
         Name = name;
         Value = value;
     }
+
+    private bool Equals(Product other)
+    {
+        return Name == other.Name && Value == other.Value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Product)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Value);
+    }
 }
