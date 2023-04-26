3e346071e6ce6864506759237fc1ca155122d7e6
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-26 09:58:53 +0100

    Feat: Put An Order With A New Product Success

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Controllers/Orders/OrdersController.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Controllers/Orders/OrdersController.cs
@@ -8,37 +8,43 @@ using OrdersWebApi.Queries;
 
 namespace OrdersWebApi.Controllers.Orders;
 
-
 [ApiController]
 [Route("[controller]")]
 public class OrdersController : ControllerBase
 {
     private readonly IClock _clock;
     private readonly CreateOrderCommand _createOrderCommand;
+    private readonly AddProductsToOrderCommand _addProductsToOrderCommand;
     private readonly GetOrderByIdQuery _getOrderByIdQuery;
 
     public OrdersController(IClock clock, CreateOrderCommand createOrderCommand,
         AddProductsToOrderCommand addProductsToOrderCommand, GetOrderByIdQuery getOrderByIdQuery)
     {
         _createOrderCommand = createOrderCommand;
+        _addProductsToOrderCommand = addProductsToOrderCommand;
         _getOrderByIdQuery = getOrderByIdQuery;
         _clock = clock;
     }
-    
+
     [HttpPost]
     public Task Post(CreateOrderRequest createOrderRequest)
     {
-        var createOrderDto = new CreateOrderDto(createOrderRequest.Id, _clock.Timestamp(), createOrderRequest.Customer, createOrderRequest.Address,
+        var createOrderDto = new CreateOrderDto(
+            createOrderRequest.Id,
+            _clock.Timestamp(),
+            createOrderRequest.Customer,
+            createOrderRequest.Address,
             createOrderRequest.Products);
         _createOrderCommand.Execute(createOrderDto);
         return Task.CompletedTask;
     }
-    
+
     [HttpPut("{id}/Products")]
     public Task Put(string id, AddProductsRequest addProductsRequest)
     {
         var addProductsDto = new AddProductsDto(id, addProductsRequest.Products);
-        return null;
+        _addProductsToOrderCommand.Execute(addProductsDto);
+        return Task.CompletedTask;
     }
 
     [HttpGet("{id}")]
