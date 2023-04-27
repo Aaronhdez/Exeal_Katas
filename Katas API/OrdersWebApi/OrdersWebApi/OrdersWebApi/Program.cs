d088911987a8e62b4c924a0cb40d223a6f420981
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-27 12:08:38 +0100

    Refactor: Added Mediatr to project references commands

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Program.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Program.cs
@@ -1,8 +1,8 @@
 using OrdersWebApi;
-using OrdersWebApi.Commands.Orders;
-using OrdersWebApi.Models.Orders;
-using OrdersWebApi.Queries;
-using OrdersWebApi.Repositories;
+using OrdersWebApi.Orders;
+using OrdersWebApi.Orders.Commands;
+using OrdersWebApi.Orders.Queries;
+using OrdersWebApi.Orders.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +13,10 @@ builder.Services.AddAuthorization();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(OrdersWebApi.Program).Assembly));
 builder.Services.AddSingleton<IClock, SystemClock>();
 builder.Services.AddSingleton<IOrderRepository, InMemoryOrdersRepository>();
-builder.Services.AddScoped<CreateOrderCommand>();
-builder.Services.AddScoped<AddProductsToOrderCommand>();
 builder.Services.AddScoped<GetOrderByIdQuery>();
 
 
@@ -34,6 +34,9 @@ app.UseAuthorization();
 app.MapControllers();
 app.Run();
 
-public partial class Program
+namespace OrdersWebApi
 {
+    public partial class Program
+    {
+    }
 }
