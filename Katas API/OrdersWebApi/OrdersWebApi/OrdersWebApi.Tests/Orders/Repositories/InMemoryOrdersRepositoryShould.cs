434ff0ecfbd39bd8e742b9d2ebaaf55c4d432700
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-28 14:06:39 +0100

    Test Update An Order Products List While Requested

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Orders/Repositories/InMemoryOrdersRepositoryShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Orders/Repositories/InMemoryOrdersRepositoryShould.cs
@@ -22,7 +22,27 @@ public class InMemoryOrdersRepositoryShould
         
         _inMemoryOrdersRepository.Create(expectedOrder);
         
-        var retrievedOrder = _inMemoryOrdersRepository.GetById("ORD123456");
+        var retrievedOrder = _inMemoryOrdersRepository.GetById("ORD123456").Result;
         retrievedOrder.Should().Be(expectedOrder);
+    }
+    
+    
+    [Test]
+    public void UpdateAnOrderProductsListWhileRequested()
+    {
+        var expectedOrder = new Order("ORD123456", "24/04/2023", "John Doe", "A Simple Street, 123",
+            new Products( new List<Product> { new("computerMonitor", 70) }));
+        var expectedUpdatedOrder = new Order("ORD123456", "24/04/2023", "John Doe", "A Simple Street, 123",
+            new Products( new List<Product>
+            {
+                new("computerMonitor", 70),
+                new("computerMonitor", 70),
+            }));
+        
+        _inMemoryOrdersRepository.Create(expectedOrder);
+        _inMemoryOrdersRepository.Update(expectedOrder);
+        
+        var retrievedOrder = _inMemoryOrdersRepository.GetById("ORD123456").Result;
+        retrievedOrder.Should().Be(expectedUpdatedOrder);
     }
 }
