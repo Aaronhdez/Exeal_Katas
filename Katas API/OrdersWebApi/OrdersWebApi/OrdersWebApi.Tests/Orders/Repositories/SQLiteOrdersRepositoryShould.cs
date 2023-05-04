3608f95c58fc949d181c044b6039aec00e32368c
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-04 16:16:11 +0100

    Refactor: Common methods extracted from tests

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Orders/Repositories/SQLiteOrdersRepositoryShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Orders/Repositories/SQLiteOrdersRepositoryShould.cs
@@ -2,12 +2,13 @@
 using Dapper;
 using FluentAssertions;
 using OrdersWebApi.Orders;
+using OrdersWebApi.Orders.Repositories;
 
 namespace OrdersWebApi.Tests.Orders.Repositories;
 
 public class SQLiteOrdersRepositoryShould
 {
-    private SQLiteOrdersRepository _inMemoryOrdersRepository;
+    private SQLiteOrdersRepository _sqLiteOrdersRepository;
     private SQLiteConnection _sqLiteConnection;
 
     [SetUp]
@@ -21,7 +22,18 @@ public class SQLiteOrdersRepositoryShould
                 CreationDate VARCHAR(100) NOT NULL,
                 Customer VARCHAR(100) NOT NULL,
                 Address VARCHAR(100) NOT NULL)");
-        _inMemoryOrdersRepository = new SQLiteOrdersRepository(_sqLiteConnection);
+        
+        await _sqLiteConnection.ExecuteAsync(
+            @"Create Table if not exists Products(
+                ID VARCHAR(100) UNIQUE,
+                Name VARCHAR(100) NOT NULL,
+                Value INTEGER NOT NULL)");
+        
+        await _sqLiteConnection.ExecuteAsync(
+            @"Create Table if not exists OrdersProducts(
+                OrderID VARCHAR(100) NOT NULL,
+                ProductID VARCHAR(100) NOT NULL)");
+        _sqLiteOrdersRepository = new SQLiteOrdersRepository(_sqLiteConnection);
     }
 
     [TearDown]
@@ -34,58 +46,64 @@ public class SQLiteOrdersRepositoryShould
     [Test]
     public void RetrieveAnOrderWithoutProductsWhileRequested()
     {
-        var expected = new Order("ORD123456", "24/04/2023", "John Doe", "A Simple Street, 123",
+        var order = new Order("ORD123456", "24/04/2023", "John Doe", "A Simple Street, 123",
             new Products(new List<Product>()));
-        _sqLiteConnection.ExecuteAsync(
-            $"INSERT INTO " +
-            $"Orders(ID, CreationDate, Customer, Address) " +
-            $"VALUES('{expected.Id}','{expected.CreationDate}','{expected.Customer}','{expected.Address}')");
+        GivenAnOrderInDB(order);
 
-        var retrievedOrder = _inMemoryOrdersRepository.GetById("ORD123456").Result;
-        
-        retrievedOrder.Should().Be(expected);
+        var retrievedOrder = _sqLiteOrdersRepository.GetById("ORD123456").Result;
+
+        retrievedOrder.Should().Be(order);
     }
-    
+
 
     [Test]
     public void InsertNewOrderWithoutProductsWhileRequested()
     {
         var expectedOrder = new Order("ORD123456", "24/04/2023", "John Doe", "A Simple Street, 123",
             new Products(new List<Product>()));
-    
-        _inMemoryOrdersRepository.Create(expectedOrder);
-    
-        var retrievedOrder = _inMemoryOrdersRepository.GetById("ORD123456").Result;
+
+        _sqLiteOrdersRepository.Create(expectedOrder);
+
+        var retrievedOrder = _sqLiteOrdersRepository.GetById("ORD123456").Result;
         retrievedOrder.Should().Be(expectedOrder);
     }
-}
 
-public class SQLiteOrdersRepository : IOrderRepository
-{
-    private readonly SQLiteConnection _connection;
-
-    public SQLiteOrdersRepository(SQLiteConnection connection)
+    [Test]
+    public void RetrieveAnListOfProductsForAnOrderWhileRequested()
     {
-        _connection = connection;
+        var product = new Product("PROD000001","Computer", 750);
+        var expectedOrder = new Order("ORD123456", "24/04/2023", "John Doe", "A Simple Street, 123",
+            new Products(new List<Product>{ product }));
+        GivenAnOrderInDB(expectedOrder);
+        GivenAProductInDB(product);
+        GivenAProductAssignedToAnOrderInDB(product.Id, expectedOrder.Id);
+        
+        var retrievedOrder = _sqLiteOrdersRepository.GetById("ORD123456").Result;
+        
+        retrievedOrder.Should().Be(expectedOrder);
     }
 
-    public Task Create(Order order)
+    private void GivenAProductAssignedToAnOrderInDB(string productId, string expectedOrderId)
     {
-        return _connection.ExecuteAsync(
+        _sqLiteConnection.ExecuteAsync(
+            $"INSERT INTO " +
+            $"OrdersProducts(OrderID, ProductID) " +
+            $"VALUES('{expectedOrderId}','{productId}')");
+    }
+
+    private void GivenAProductInDB(Product product)
+    {
+        _sqLiteConnection.ExecuteAsync(
+            $"INSERT INTO " +
+            $"Products(ID, [Name], [Value]) " +
+            $"VALUES('{product.Id}', '{product.Name}','{product.Value}')");
+    }
+
+    private void GivenAnOrderInDB(Order expectedOrder)
+    {
+        _sqLiteConnection.ExecuteAsync(
             $"INSERT INTO " +
             $"Orders(ID, CreationDate, Customer, Address) " +
-            $"VALUES('{order.Id}','{order.CreationDate}','{order.Customer}','{order.Address}')");
-    }
-
-    public Task<Order> GetById(string id)
-    {
-         var queryResult = _connection.QueryFirstOrDefaultAsync<dynamic?>($"SELECT * FROM Orders WHERE ID = '{id}'").Result;
-         var retrievedOrder = new Order(queryResult.ID, queryResult.CreationDate, queryResult.Customer, queryResult.Address, new Products(new List<Product>()));
-         return Task.FromResult(retrievedOrder);
-    }
-
-    public Task Update(Order orderModel)
-    {
-        throw new NotImplementedException();
+            $"VALUES('{expectedOrder.Id}','{expectedOrder.CreationDate}','{expectedOrder.Customer}','{expectedOrder.Address}')");
     }
 }
