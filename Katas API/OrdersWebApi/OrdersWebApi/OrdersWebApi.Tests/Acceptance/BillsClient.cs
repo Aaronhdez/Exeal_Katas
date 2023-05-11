7359b5e6c56223e35df9d7e78a2e7f67e0ed5388
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-11 14:23:20 +0100

    Test: Get Order Bill By Id

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Acceptance/BillsClient.cs
@@ -0,0 +1,16 @@
+namespace OrdersWebApi.Tests.Acceptance;
+
+public class BillsClient {
+    private readonly HttpClient _client;
+
+    public BillsClient(HttpClient client) {
+        _client = client;
+    }
+
+    public async Task<string> GetOrderBillById(string orderId) {
+        var getResponse = await _client.GetAsync($"/Bills/{orderId}");
+        getResponse.EnsureSuccessStatusCode();
+        var content = await getResponse.Content.ReadAsStringAsync();
+        return content;
+    }
+}
