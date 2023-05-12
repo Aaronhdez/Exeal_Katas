eda2f8b51b1c45b6b9594bd76dec971d78ee7432
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-12 13:57:55 +0100

    Test: Display A Created Product

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Acceptance/DisplayProductFeature.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using NSubstitute;
+using OrdersWebApi.Infrastructure;
+using OrdersWebApi.Products;
+using OrdersWebApi.Tests.Orders;
+
+namespace OrdersWebApi.Tests.Acceptance;
+
+public class DisplayProductFeature {
+    private IClock? _clock;
+    private OrdersApi _ordersApi;
+    private IGuidGenerator _idGenerator;
+    private HttpClient _client;
+
+    [SetUp]
+    public void SetUp() {
+        _clock = Substitute.For<IClock>();
+        _clock.Timestamp().Returns(TestDefaultValues.CreationDateTime);
+        _idGenerator = Substitute.For<IGuidGenerator>();
+        _idGenerator.NewId().Returns(TestDefaultValues.OrderGuid);
+        _ordersApi = new OrdersApi(_clock, _idGenerator);
+        _client = _ordersApi.CreateClient();
+    }
+
+    [Test]
+    public async Task DisplayACreatedProduct() {
+        var createProductDto = GivenAProductCreationDto();
+        
+        var id = await WhenUserRequestsToCreateIt(createProductDto);
+        
+        await ThenTheProductIsCreatedProperly(id);
+    }
+
+    private string GivenAProductCreationDto() {
+        var productCreationDto = new CreateProductDto("MON",
+            "Computer Monitor",
+            "A description",
+            "A Manufacturer",
+            "A Reference",
+            100);
+        return JsonConvert.SerializeObject(productCreationDto);
+    }
+
+    private async Task<string> WhenUserRequestsToCreateIt(string createProductDto) {
+        var postResponse = await _client.PostAsync("/Products", new StringContent(createProductDto, Encoding.Default, "application/json"));
+        postResponse.EnsureSuccessStatusCode();
+        var content = await postResponse.Content.ReadAsStringAsync();
+        return content;
+    }
+
+    private async Task ThenTheProductIsCreatedProperly(string id) {
+        var getResponse = await _client.GetAsync($"/Products/{id}");
+        getResponse.EnsureSuccessStatusCode();
+        var content = await getResponse.Content.ReadAsStringAsync();
+        var json = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(content), Formatting.Indented);
+        Verify(json);
+    }
+}
---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Products/CreateProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OrdersWebApi.Products;
+
+public record CreateProductDto(
+    string type,
+    string name,
+    string decription,
+    string manufacturer,
+    string manufacturerReference,
+    int value
+);
