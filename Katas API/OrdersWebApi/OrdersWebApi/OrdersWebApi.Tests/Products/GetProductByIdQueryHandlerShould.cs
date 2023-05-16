ce8c58da8c8f942861de7576b6ba6fd56fd5ad29
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-16 14:17:19 +0100

    Feat: Get A Product Dto From Existing Record

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Products/GetProductByIdQueryHandlerShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Products/GetProductByIdQueryHandlerShould.cs
@@ -2,18 +2,19 @@
 using NSubstitute.Core;
 using NSubstitute.ExceptionExtensions;
 using OrdersWebApi.Orders;
+using OrdersWebApi.Products;
 
 namespace OrdersWebApi.Tests.Products; 
 
 public class GetProductByIdQueryHandlerShould {
     private GetProductByIdQuery _getOrderByIdQuery;
     private GetProductByIdQueryHandler _handler;
-    private IProductsRepository _ordersRepository;
+    private IProductsRepository _repository;
 
     [SetUp]
     public void SetUp() {
-        _ordersRepository = new InMemoryProductsRepository();
-        _handler = new GetProductByIdQueryHandler(_ordersRepository);
+        _repository = new InMemoryProductsRepository();
+        _handler = new GetProductByIdQueryHandler(_repository);
     }
 
     [Test]
@@ -24,6 +25,26 @@ public class GetProductByIdQueryHandlerShould {
 
         result.Should().Throw<ArgumentException>();
     }
-    
-    
+
+    [Test]
+    public async Task GetAProductDtoFromExistingRecord() {
+        var productId = "An Id";
+        var product = new Item(productId, "MON", "A Name", "A Description",
+            "A Manufacturer", "A Manufacturer Reference", 0);
+        await _repository.Create(product);
+
+        var productQuery = new GetProductByIdQuery(productId);
+        var result = await _handler.Handle(productQuery, default);
+        
+        var expectedDto = new ProductReadDto(
+            productId, 
+            "MON", 
+            "A Name", 
+            "A Description",
+            "A Manufacturer", 
+            "A Manufacturer Reference", 
+            0);
+        result.Should().BeEquivalentTo(expectedDto);
+    }
+
 }
