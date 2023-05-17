c7625cb612147b0ec72d2769ec6238899a3d0bef
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-17 08:39:23 +0100

    Refactor: method extracted in format generator

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Infrastructure/ProductReferenceGenerator.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Infrastructure/ProductReferenceGenerator.cs
@@ -11,6 +11,10 @@ public class ProductReferenceGenerator {
 
     public async Task<string> GenerateReferenceForTag(string tag) {
         var taggedProducts = await _repository.GetAllProductsForTag(tag);
+        return GetFormattedReference(taggedProducts);
+    }
+
+    private static string GetFormattedReference(IEnumerable<Product> taggedProducts) {
         if (taggedProducts.Count() == 0)
             return "MON000001";
         return taggedProducts.Count() == 2 ? "MON000003" : "MON000002";
