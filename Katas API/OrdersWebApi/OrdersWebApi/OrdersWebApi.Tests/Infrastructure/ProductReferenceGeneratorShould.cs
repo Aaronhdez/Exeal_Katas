fd7066f72d6298b9eea04f452a6df65d18272dbf
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-17 09:04:55 +0100

    Refactor: test refactored due to clean up solution

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Infrastructure/ProductReferenceGeneratorShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Infrastructure/ProductReferenceGeneratorShould.cs
@@ -4,72 +4,63 @@ using OrdersWebApi.Infrastructure;
 using OrdersWebApi.Products;
 using OrdersWebApi.Products.Repositories;
 
-namespace OrdersWebApi.Tests.Infrastructure; 
+namespace OrdersWebApi.Tests.Infrastructure;
 
 public class ProductReferenceGeneratorShould {
     private IProductsRepository _repository;
     private ProductReferenceGenerator _referenceGenerator;
+    private ICollection<Product> _dummyList;
+
+    [SetUp]
+    public void SetUp() {
+        _repository = Substitute.For<IProductsRepository>();
+        _dummyList = Substitute.For<ICollection<Product>>();
+        _repository.GetAllProductsForTag(Arg.Any<string>()).Returns(_dummyList);
+        _referenceGenerator = new ProductReferenceGenerator(_repository);
+    }
 
     [Test]
     public async Task GenerateAnIdForFirstOccurenceInARepository() {
-        _repository = new InMemoryProductsRepository();
-        _referenceGenerator = new ProductReferenceGenerator(_repository);
+        _dummyList.Count().Returns(0);
 
         var reference = await _referenceGenerator.GenerateReferenceForTag("MON");
 
-        var expectedReference = "MON000001";
-        reference.Should().Be(expectedReference);
+        reference.Should().Be("MON000001");
     }
-    
+
     [Test]
     public async Task GenerateAnIdForSecondOccurenceInARepository() {
-        _repository = new InMemoryProductsRepository();
-        _referenceGenerator = new ProductReferenceGenerator(_repository);
-        await _repository.Create(TestDefaultValues.ComputerMonitor);
+        _dummyList.Count().Returns(1);
 
         var reference = await _referenceGenerator.GenerateReferenceForTag("MON");
 
-        var expectedReference = "MON000002";
-        reference.Should().Be(expectedReference);
+        reference.Should().Be("MON000002");
     }
-    
+
     [Test]
     public async Task GenerateAnIdForThirdOccurenceInARepository() {
-        _repository = new InMemoryProductsRepository();
-        _referenceGenerator = new ProductReferenceGenerator(_repository);
-        await _repository.Create(TestDefaultValues.ComputerMonitor);
-        await _repository.Create(TestDefaultValues.AnotherComputerMonitor);
+        _dummyList.Count().Returns(2);
 
         var reference = await _referenceGenerator.GenerateReferenceForTag("MON");
 
-        var expectedReference = "MON000003";
-        reference.Should().Be(expectedReference);
+        reference.Should().Be("MON000003");
     }
 
     [Test]
     public async Task GenerateAnIdFor10Ocurrences() {
-        _repository = Substitute.For<IProductsRepository>();
-        _referenceGenerator = new ProductReferenceGenerator(_repository);
-        _repository.GetAllProductsForTag(Arg.Any<string>()).Returns(new List<Product> {
-            new(), new(), new(), new(), new(), new(), new(), new(), new(),
-        });
+        _dummyList.Count().Returns(9);
+
         var reference = await _referenceGenerator.GenerateReferenceForTag("MON");
 
-        var expectedReference = "MON000010";
-        reference.Should().Be(expectedReference);
+        reference.Should().Be("MON000010");
     }
-    
+
     [Test]
     public async Task GenerateAnIdFor100Ocurrences() {
-        _repository = Substitute.For<IProductsRepository>();
-        var dummyList = Substitute.For<ICollection<Product>>();
-        dummyList.Count().Returns(99);
-        _repository.GetAllProductsForTag(Arg.Any<string>()).Returns(dummyList);
-        _referenceGenerator = new ProductReferenceGenerator(_repository);
-        
+        _dummyList.Count().Returns(99);
+
         var reference = await _referenceGenerator.GenerateReferenceForTag("MON");
 
-        var expectedReference = "MON000100";
-        reference.Should().Be(expectedReference);
+        reference.Should().Be("MON000100");
     }
 }
