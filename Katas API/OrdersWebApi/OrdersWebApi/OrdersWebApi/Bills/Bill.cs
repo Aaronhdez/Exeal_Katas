45007ec584f8fd78a90251b921f6bdcab4deab7e
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-19 13:33:36 +0100

    Refactor: Introduced Bill entity

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Bills/Bill.cs
@@ -0,0 +1,52 @@
+﻿using OrdersWebApi.Orders;
+using OrdersWebApi.Products;
+
+namespace OrdersWebApi.Bills.Queries;
+
+public class Bill {
+    private string Company { get; }
+    private string CompanyAddress { get; }
+    private string Customer { get; }
+    private string CustomerAddress { get; }
+    private IEnumerable<BillRow> Items { get; }
+    private int Total { get; }
+
+    public Bill(Order order) {
+        Company = "Computer Stuff Inc.";
+        CompanyAddress = "A company Address";
+        Customer = order.Customer;
+        CustomerAddress = order.Address;
+        Items = GetItemsAsList(GetItemsAssociated(order), out var total);
+        Total = total;
+    }
+
+    public ReadBillDto ToReadDto() {
+        return new ReadBillDto(Company, CompanyAddress, Customer, CustomerAddress, Items, Total);
+    }
+
+    private static Dictionary<Product, int> GetItemsAssociated(Order order) {
+        var itemsAssociated = new Dictionary<Product, int>();
+        foreach (var item in order.Products) {
+            if (itemsAssociated.ContainsKey(item)) {
+                itemsAssociated[item] += 1;
+            } else {
+                itemsAssociated.Add(item, 1);
+            }
+        }
+        return itemsAssociated;
+    }
+
+    private IEnumerable<BillRow> GetItemsAsList(Dictionary<Product, int> itemsAssociated, out int total) {
+        var summarizedProducts = new List<BillRow>();
+        total = 0;
+        foreach (var item in itemsAssociated) {
+            var currentItemValue = item.Value * item.Key.Value;
+            summarizedProducts.Add(new BillRow (
+                item.Value + " x "+item.Key.Name+"",
+                currentItemValue
+            ));
+            total += currentItemValue;
+        }
+        return summarizedProducts;
+    }
+}
--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Bills/Queries/GetBillByOrderIdQueryHandler.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Bills/Queries/GetBillByOrderIdQueryHandler.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using OrdersWebApi.Orders;
-using OrdersWebApi.Products;
 
 namespace OrdersWebApi.Bills.Queries; 
 
@@ -14,44 +13,6 @@ public class GetBillByOrderIdQueryHandler : IRequestHandler<GetBillByOrderIdQuer
     
     public Task<ReadBillDto> Handle(GetBillByOrderIdQuery request, CancellationToken cancellationToken) {
         var order = _ordersRepository.GetById(request.Id).Result;
-        var itemsAssociated = GetItemsAssociated(order);
-        return Task.FromResult(CreateReadBillDto(order, itemsAssociated));
-    }
-
-    private static Dictionary<Product, int> GetItemsAssociated(Order order) {
-        var itemsAssociated = new Dictionary<Product, int>();
-        foreach (var item in order.Products) {
-            if (itemsAssociated.ContainsKey(item)) {
-                itemsAssociated[item] += 1;
-            } else {
-                itemsAssociated.Add(item, 1);
-            }
-        }
-        return itemsAssociated;
-    }
-
-    private ReadBillDto CreateReadBillDto(Order order, Dictionary<Product, int> itemsAssociated) {
-        return new ReadBillDto(
-            "Computer Stuff Inc.",
-            "A company Address",
-            order.Customer,
-            order.Address,
-            GetItemsAsList(itemsAssociated, out var total),
-            total
-        );
-    }
-
-    private IEnumerable<BillRow> GetItemsAsList(Dictionary<Product, int> itemsAssociated, out int total) {
-        var summarizedProducts = new List<BillRow>();
-        total = 0;
-        foreach (var item in itemsAssociated) {
-            var currentItemValue = item.Value * item.Key.Value;
-            summarizedProducts.Add(new BillRow (
-                item.Value + " x "+item.Key.Name+"",
-                currentItemValue
-            ));
-            total += currentItemValue;
-        }
-        return summarizedProducts;
+        return Task.FromResult(new Bill(order).ToReadDto());
     }
 }
