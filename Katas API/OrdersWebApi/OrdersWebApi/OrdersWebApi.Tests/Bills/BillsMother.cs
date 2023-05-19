70687bd626886c4210627a6aa6580180661ae013
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-19 13:00:41 +0100

    Refactor: BillsMother.cs extracted to get prebuild objects

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Bills/BillsMother.cs
@@ -0,0 +1,18 @@
+﻿using OrdersWebApi.Bills.Queries;
+
+namespace OrdersWebApi.Tests.Bills;
+
+public class BillsMother {
+    public static ReadBillDto ATestBill() {
+        return new ReadBillDto (
+            TestDefaultValues.CompanyName,
+            TestDefaultValues.CompanyAddress,
+            TestDefaultValues.CustomerName,
+            TestDefaultValues.CustomerAddress,
+            new List<BillRow> {
+                new("1 x Computer Monitor", 100 )
+            },
+            100
+        );
+    }
+}
