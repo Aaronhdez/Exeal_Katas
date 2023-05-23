de3de77b42335bf6574b02da3c08123b1fa33db6
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-23 15:42:48 +0100

    Feat: Retrieve An Existent User

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/GetUserByIdQueryShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/GetUserByIdQueryShould.cs
@@ -9,12 +9,11 @@ public class GetUserByIdQueryShould {
     [Test]
     public async Task RetrieveAnExistentUser() {
         _usersRepository = new InMemoryUsersRepository();
-        var userId = "An Id";
-        var existentUser = new User(userId, "A Name", "An Address");
+        var existentUser = new User("An Id", "A Name", "An Address");
         _usersRepository.Create(existentUser);
         var handler = new GetUserByIdQueryHandler(_usersRepository);
 
-        var retrievedUser = await handler.Handle(new GetUserByIdQuery(userId), default);
+        var retrievedUser = await handler.Handle(new GetUserByIdQuery("An Id"), default);
 
         var expectedUser = new ReadUserDto(existentUser.Id, existentUser.Name, existentUser.Address);
         retrievedUser.Should().BeEquivalentTo(expectedUser);
