ff1008ea6f5de1db6054604d2582ce026da736dc
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-23 15:37:43 +0100

    Test: Retrieve A User If Exists

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/UsersRepositoryShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/UsersRepositoryShould.cs
@@ -13,4 +13,15 @@ public class UsersRepositoryShould {
         
         user.Should().BeNull();
     }
+    
+    [Test]
+    public void RetrieveAUserIfExists() {
+        _usersRepository = new InMemoryUsersRepository();
+        var expectedUser = new User("AnId", "A Name", "An Address");
+        _usersRepository.Create(expectedUser);
+        
+        var user = _usersRepository.GetById("AnId");
+        
+        user.Should().BeEquivalentTo(expectedUser);
+    }
 }
