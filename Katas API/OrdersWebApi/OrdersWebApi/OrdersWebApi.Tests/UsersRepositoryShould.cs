8bcaa390367e102776cbe0e488a32cd0f09261eb
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-23 15:36:08 +0100

    Feat: Retrieve Null When As User Does Not Exist

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/UsersRepositoryShould.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+
+namespace OrdersWebApi.Tests; 
+
+public class UsersRepositoryShould {
+    private IUserRepository _usersRepository;
+
+    [Test]
+    public void RetrieveNullWhenAsUserDoesNotExist() {
+        _usersRepository = new InMemoryUsersRepository();
+        
+        var user = _usersRepository.GetById("AnId");
+        
+        user.Should().BeNull();
+    }
+}
