5139b3fe7b8262c3d5f834d569cebc510ea5cdad
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-25 16:05:11 +0100

    Refactor: Address validation extracted to its own class

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Users/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace OrdersWebApi.Users;
+
+public class Address {
+    public Address(string value) {
+        if (string.IsNullOrEmpty(value)) throw new ArgumentException();
+        Value = value;
+    }
+    public string Value { get; }
+}
--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Users/User.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi/Users/User.cs
@@ -2,24 +2,12 @@
 
 namespace OrdersWebApi.Users;
 
-public class Address {
-    public Address(string value) {
-        Value = value;
-    }
-    public string Value { get; }
-}
-
 public class User {
     public User(string id, UserData userData, Address address) {
-        ValidateData(id, userData.Name, address.Value);
+        if (string.IsNullOrEmpty(id)) throw new ArgumentException();
         Id = id;
         Name = userData.Name;
         Address = address.Value;
-    }
-
-    private static void ValidateData(string id, string name, string address) {
-        if (string.IsNullOrEmpty(id)) throw new ArgumentException();
-        if (string.IsNullOrEmpty(address)) throw new ArgumentException();
     }
 
     public string Name { get; }
