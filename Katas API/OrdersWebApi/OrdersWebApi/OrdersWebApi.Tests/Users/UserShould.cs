cb2cb0812cc4c84383399dc32bc4269e783aa4aa
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-25 15:45:14 +0100

    Test: Not Be Created If Guid Is Null

---
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Users/UserShould.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using OrdersWebApi.Users;
+
+namespace OrdersWebApi.Tests.Users; 
+
+public class UserShould {
+
+    [Test]
+    public void NotBeCreatedIfGuidIsNull() {
+        var action = () => new User(null, "A User", "An Address");
+
+        action.Should().Throw<ArgumentException>();
+    }
+}
