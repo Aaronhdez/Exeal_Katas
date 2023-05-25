2edb3f3dbcb5893a98527af8758c1766163c31bc
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-25 15:54:29 +0100

    Test: Not Be Created If Address Is Null

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Users/UserShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Users/UserShould.cs
@@ -32,4 +32,11 @@ public class UserShould {
 
         action.Should().Throw<ArgumentException>();
     }
+
+    [Test]
+    public void NotBeCreatedIfAddressIsNull() {
+        var action = () => new User(UserDefaultValues.CustomerId, UserDefaultValues.CustomerName, null);
+
+        action.Should().Throw<ArgumentException>();
+    }
 }
