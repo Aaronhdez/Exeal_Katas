b5f0f0f54509d9717666254a59582dced30c06cd
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-05-25 15:51:31 +0100

    Test: Not Be Created If Name Is Null

--- Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Users/UserShould.cs
+++ Katas API/OrdersWebApi/OrdersWebApi/OrdersWebApi.Tests/Users/UserShould.cs
@@ -1,20 +1,27 @@
 ﻿using FluentAssertions;
+using OrdersWebApi.TestUtils.Users;
 using OrdersWebApi.Users;
 
-namespace OrdersWebApi.Tests.Users; 
+namespace OrdersWebApi.Tests.Users;
 
 public class UserShould {
-
     [Test]
     public void NotBeCreatedIfGuidIsNull() {
-        var action = () => new User(null, "A User", "An Address");
+        var action = () => new User(null, UserDefaultValues.CustomerName, UserDefaultValues.CustomerAddress);
 
         action.Should().Throw<ArgumentException>();
     }
-    
+
     [Test]
     public void NotBeCreatedIfGuidIsEmpty() {
-        var action = () => new User("", "A User", "An Address");
+        var action = () => new User(string.Empty, UserDefaultValues.CustomerName, UserDefaultValues.CustomerAddress);
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void NotBeCreatedIfNameIsNull() {
+        var action = () => new User(UserDefaultValues.CustomerId, null, UserDefaultValues.CustomerAddress);
 
         action.Should().Throw<ArgumentException>();
     }
